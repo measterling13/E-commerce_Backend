420e937f626263d792048046635ad80aaf8a62d7
Tigro <easterlingme95@gmail.com>
2023-02-03 13:44:19 -0600

    added find category by id route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -10,8 +10,21 @@ router.get('/', (req, res) => {
 });
 
 router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+  Category.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: {
+      model: Product,
+      attributes: ['id', 'product_name', 'price', 'stock', 'category_id']
+    }
+    }).then((categoryData) => {
+      if(!categoryData) {
+        res.status(404).json({message: 'No Categorie found'});
+        return;
+      }
+    res.json(categoryData);
+  });
 });
 
 router.post('/', (req, res) => {

e90b96738745624f37c97b7f141230a34f37221d
Tigro <easterlingme95@gmail.com>
2023-02-03 13:58:27 -0600

    added the delete tag route

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -63,7 +63,20 @@ router.put('/:id', (req, res) => {
 });
 
 router.delete('/:id', (req, res) => {
-  // delete on tag by its `id` value
+  Tag.destroy({
+    where: {
+      id: req.params.id,
+    },
+  }).then(tagData => {
+    if(!tagData) {
+      res.status(404).json({message: 'no tag found at this time'});
+      return;
+    }
+    res.json(tagData);
+  }).catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 module.exports = router;

62f901ffa708f4e68dc48fef94d9641479610aaf
Tigro <easterlingme95@gmail.com>
2023-02-03 13:47:55 -0600

    added the delete category route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -57,7 +57,15 @@ router.put('/:id', (req, res) => {
 });
 
 router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
+  Category.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((deleteCategory) => {
+      res.json(deleteCategory);
+    })
+    .catch((err) => res.status(500).json(err));
 });
 
 module.exports = router;
